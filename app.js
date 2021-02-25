7d5812f71f6aca651357063e8c117939d9269008
Ekho26 <munteanu.ionut1991@gmail.com>
2021-02-25 11:55:53 +0200

    Adding timer and collision

--- app.js
+++ app.js
@@ -21,22 +21,72 @@ document.addEventListener("DOMContentLoaded", () => {
 
   const tTetromino = [
     [1, width, width + 1, width + 2],
-    [1, width + 1, width + 2, width * 2+1],
+    [1, width + 1, width + 2, width * 2 + 1],
     [width, width + 1, width + 2, width * 2 + 1],
     [1, width, width + 1, width * 2 + 1],
   ];
 
   const oTetromino = [
-      [0,1,width,width+1],
-      [0,1,width,width+1],
-      [0,1,width,width+1],
-      [0,1,width,width+1]
-  ]
+    [0, 1, width, width + 1],
+    [0, 1, width, width + 1],
+    [0, 1, width, width + 1],
+    [0, 1, width, width + 1],
+  ];
 
-  const itetromino = [
+  const iTetromino = [
     [1, width + 1, width * 2 + 1, width * 3 + 1],
     [width, width + 1, width + 2, width + 3],
     [1, width + 1, width * 2 + 1, width * 3 + 1],
     [width, width + 1, width + 2, width + 3],
   ];
+
+  const fiveTetromino = [
+    lTetromino,
+    zTetromino,
+    tTetromino,
+    oTetromino,
+    iTetromino,
+  ];
+
+  let currentPosition = 4;
+  let currentRotation = 0;
+  let random = Math.floor(Math.random() * fiveTetromino.length);
+  let current = fiveTetromino[random][0];
+
+  function draw() {
+    current.forEach((index) => {
+      squares[currentPosition + index].classList.add("tetromino");
+    });
+  }
+
+  function undraw() {
+    current.forEach((index) => {
+      squares[currentPosition + index].classList.remove("tetromino");
+    });
+  }
+
+  setTimer = setInterval(moveDown, 500);
+
+  function moveDown() {
+    undraw();
+    currentPosition += width;
+    draw();
+    stop();
+  }
+
+  function stop() {
+    if (
+      current.some((index) =>
+        squares[currentPosition + index + width].classList.contains("taken")
+      )
+    ) {
+      current.forEach((index) =>
+        squares[currentPosition + index].classList.add("taken")
+      );
+      random = Math.floor(Math.random() * fiveTetromino.length);
+      current = fiveTetromino[random][currentRotation];
+      currentPosition = 4;
+      draw();
+    }
+  }
 });
