14461cdbfbeca60b9bbe0132409d92336d332469
Ekho26 <munteanu.ionut1991@gmail.com>
2021-02-25 11:05:28 +0200

    Adding L tetromino

--- app.js
+++ app.js
@@ -1,3 +1,19 @@
 document.addEventListener('DOMContentLoaded',() => {
+
+    const width = 10;
+    const grid = document.querySelector('.grid');
+    let squares = Array.from(document.querySelectorAll('.grid div'));
+    const scoreDisplay = document.querySelector('#score');
+    const startBtn = document.querySelector('#start-button');
     
+    const lTetromino = [
+      [1, width + 1, width * 2 + 1, 2],
+      [width, width + 1, width + 2, width * 2 + 2],
+      [1, width + 1, width * 2 + 1, width*2],
+      [width, width*2,width*2+1,width*2+2],
+    ];
+
+    const zTetromino = [
+        []
+    ]
 })
