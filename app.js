4882b747818fb201e577765531919032273b12bf
Ekho26 <munteanu.ionut1991@gmail.com>
2021-02-25 11:21:32 +0200

    Adding the O tetromino

--- app.js
+++ app.js
@@ -21,10 +21,17 @@ document.addEventListener("DOMContentLoaded", () => {
 
   const tTetromino = [
     [1, width, width + 1, width + 2],
-    [1, width + 1, width + 2, width * 2],
+    [1, width + 1, width + 2, width * 2+1],
     [width, width + 1, width + 2, width * 2 + 1],
     [1, width, width + 1, width * 2 + 1],
   ];
+
+  const oTetromino = [
+      [0,1,width,width+1],
+      [0,1,width,width+1],
+      [0,1,width,width+1],
+      [0,1,width,width+1]
+  ]
 
   const itetromino = [
     [1, width + 1, width * 2 + 1, width * 3 + 1],
