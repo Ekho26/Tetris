da22d1728476e4cd7d1614bcfb80aa742c597c74
Ekho26 <munteanu.ionut1991@gmail.com>
2021-02-25 11:11:16 +0200

    Adding T tetromino

--- app.js
+++ app.js
@@ -1,26 +1,28 @@
-document.addEventListener('DOMContentLoaded',() => {
+document.addEventListener("DOMContentLoaded", () => {
+  const width = 10;
+  const grid = document.querySelector(".grid");
+  let squares = Array.from(document.querySelectorAll(".grid div"));
+  const scoreDisplay = document.querySelector("#score");
+  const startBtn = document.querySelector("#start-button");
 
-    const width = 10;
-    const grid = document.querySelector('.grid');
-    let squares = Array.from(document.querySelectorAll('.grid div'));
-    const scoreDisplay = document.querySelector('#score');
-    const startBtn = document.querySelector('#start-button');
-    
-    const lTetromino = [
-      [1, width + 1, width * 2 + 1, 2],
-      [width, width + 1, width + 2, width * 2 + 2],
-      [1, width + 1, width * 2 + 1, width*2],
-      [width, width*2,width*2+1,width*2+2],
-    ];
+  const lTetromino = [
+    [1, width + 1, width * 2 + 1, 2],
+    [width, width + 1, width + 2, width * 2 + 2],
+    [1, width + 1, width * 2 + 1, width * 2],
+    [width, width * 2, width * 2 + 1, width * 2 + 2],
+  ];
 
-    const zTetromino = [
-      [width * 2, width * 2 + 1, width + 1, width + 2],
-      [0, width, width + 1, width * 2],
-      [width * 2, width * 2 + 1, width + 1, width + 2],
-      [0, width, width + 1, width * 2],
-    ];
+  const zTetromino = [
+    [width * 2, width * 2 + 1, width + 1, width + 2],
+    [0, width, width + 1, width * 2],
+    [width * 2, width * 2 + 1, width + 1, width + 2],
+    [0, width, width + 1, width * 2],
+  ];
 
-    const tTetromino = [
-        
-    ]
-})
+  const tTetromino = [
+    [1, width, width + 1, width + 2],
+    [1, width + 1, width + 2, width * 2],
+    [width, width + 1, width + 2, width * 2 + 1],
+    [1, width, width + 1, width * 2 + 1],
+  ];
+});
